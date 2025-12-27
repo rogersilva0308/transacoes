f734b1d8f24f8742d4da6dbcc194ebf708fe8843
rogersilva <rogersilva0308@gmail.com>
2025-12-27 15:06:03 -0300

    Criando Projeto Transações

---
+++ src/test/java/com/rogerio/transacoes/TransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogerio.transacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
